2d722d818ac58a69cb2d614285ff26ef6a00589e
mattbeedle <mattbeedle@gmail.com>
2016-06-17 21:55:08 +0700

    wtf

--- ext/faster_path/extconf.rb
+++ ext/faster_path/extconf.rb
@@ -12,8 +12,8 @@ unless find_executable('cargo')
 end
 
 Dir.chdir(File.expand_path("../../", File.dirname(__FILE__))) do
-  %x(rake build_src)
-  %x(rake clean_src)
+  `rake build_src`
+  `rake clean_src`
 end
 
 create_makefile('faster_path/dummy')
